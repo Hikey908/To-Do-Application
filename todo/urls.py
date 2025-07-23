a479c7f5dc4c17a12057fde2859f62d9849ca90e
Hassan Shaikh <hassanshailh908@gmail.com>
2025-07-23 20:05:36 +0530

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.signup),
+    path('loginn', views.loginn, name='loginn'),
+    path('todopage/',views.todo),
+    path('signout/', views.signout, name='signout'),
+    path('edit_todo/<int:srno>', views.edit_todo, name='edit_todo'),
+    path('delete_todo/<int:srno>', views.delete_todo, name='delete_todo'),
+]
